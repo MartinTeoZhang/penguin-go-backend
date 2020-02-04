dd23c8a05ff8344100e4644a925310e892d576bf
Ryzin <914571420@qq.com>
2020-02-04 13:15:49 +0800

    Spring Initializr生成Spring Boot项目模板，搭建开发环境

---
+++ penguin-admin/src/main/java/com/ryzin/penguin/admin/PenguinAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ryzin.penguin.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenguinAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenguinAdminApplication.class, args);
+	}
+
+}
---
+++ penguin-admin/src/test/java/com/ryzin/penguinadmin/PenguinAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryzin.penguinadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenguinAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
