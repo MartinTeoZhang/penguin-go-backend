437e361d58c2b3bb83ef1cc2fa069ba1aedc6be5
Ryzin <914571420@qq.com>
2020-04-03 10:05:27 +0800

    服务熔断、熔断监控面板和熔断监控集群（Hystrix Dashboard、Turbine）
    测试服务提供者停用和恢复时的熔断机制
    测试单体Hystrix消费者
    /hystrix.stream
    Turbine聚合多个Hytrix服务消费者的信息显示
    /turbine.stream

--- penguin-consumer/src/main/java/com/ryzin/penguin/consumer/feign/PenguinProducerService.java
+++ penguin-consumer/src/main/java/com/ryzin/penguin/consumer/feign/PenguinProducerService.java
@@ -3,7 +3,7 @@ package com.ryzin.penguin.consumer.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "penguin-producer", fallback = FeignHelloHystrix.class) // penguin-producer是要调用的服务名
+@FeignClient(name = "penguin-producer", fallback = PenguinProducerHystrix.class) // penguin-producer 是要调用的服务名，fallback 绑定回调类
 public interface PenguinProducerService { // Feign是声明式调用
 	
     @RequestMapping("/hello") // 添加跟调用目标方法一样的方法声明，只需要方法声明，不需要具体实现，注意跟目标方法定义保持一致
---
+++ penguin-hystrix/src/main/java/com/ryzin/penguin/hystrix/PenguinHystrixApplication.java
@@ -0,0 +1,21 @@
+package com.ryzin.penguin.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * 启动器
+ */
+@EnableTurbine
+@EnableHystrixDashboard
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PenguinHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenguinHystrixApplication.class, args);
+	}
+}
